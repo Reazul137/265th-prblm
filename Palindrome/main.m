1eac609f77a73b17465401d7db336d733eb670a2
Your Name <reazulislam137@gmail.com>
2018-05-08 21:02:58 +0600

    first commit

--- Palindrome/main.m
+++ Palindrome/main.m
@@ -6,12 +6,52 @@
 //  Copyright © 2018 Reazul Islam. All rights reserved.
 //
 
+
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+@interface SampleClass:NSObject
+/* method declaration */
+- (int)max:(int)num1;
+
+@end
+
+@implementation SampleClass
+
+/* method returning the max between two numbers */
+- (int)max:(int)num1{
+    /* local variable declaration */
+    int p, q=10, r=num1%10;
+    num1 = num1 / 10;
+    while (num1 != 0)
+    {
+        p = num1 % 10;
+        r = q * r + p;
+        num1=num1 / 10;
     }
+        
+    return r;
+}
+
+@end
+
+int main ()
+{
+    /* local variable definition */
+    int a;
+    int ret;
+    
+    
+    SampleClass *sampleClass = [[SampleClass alloc]init];
+    scanf("%d",&a);
+    
+    /* calling a method to get max value */
+    ret = [sampleClass max:a];
+    
+    if (ret == a )
+    {
+        NSLog(@"Palindrome");
+    }
+    else
+        NSLog(@"Not Palindrome\n");
     return 0;
 }
