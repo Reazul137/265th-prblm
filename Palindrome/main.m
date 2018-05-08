d098b2ced88128523ae43f69a4adaffb95a9c12a
Reazul Islam <reazulislam@reazuls-mac.local>
2018-05-07 23:43:46 -0700

    Initial Commit

---
+++ Palindrome/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Palindrome
+//
+//  Created by Reazul Islam on 5/7/18.
+//  Copyright © 2018 Reazul Islam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
